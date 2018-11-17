418d34528ad82c308f7ec15019a60655369a6be5
Cole Faloon <cfaloon@gmail.com>
2018-11-17 13:39:01 -0500

    Add two new tests.

--- doubler_test.rb
+++ doubler_test.rb
@@ -6,8 +6,19 @@ class DoublerTest < Minitest::Test
     assert Doubler.new(5).is_a?(Doubler)
   end
 
+  def test_initialization_errors_for_non_numbers
+    assert_raises ArgumentError do
+       Doubler.new('a')
+     end
+  end
+
   def test_double
     doubler = Doubler.new(5)
     assert_equal doubler.double, 10
+  end
+
+  def test_negative_number
+    doubler = Doubler.new(-5)
+    assert_equal doubler.double, -10
   end
 end
